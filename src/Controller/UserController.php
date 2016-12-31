8a0fcf60cfc5f105a277441acf3d2b8467a95108
Opaleye Olabode Gabriel <Opaleye Olabode Gabriel>
2016-12-31 16:05:31 +0100

    Convert tabs to spaces

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,16 +6,16 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 class UserController 
 {
-	public function getUsers(Request $request, Response $response)
-	{
-		$users = [
-		    'Ade',
-		    'Joke',
-		    'Gbenga',
-		    'Timi',
-		    'Ajayi',
-		];
+    public function getUsers(Request $request, Response $response)
+    {
+        $users = [
+            'Ade',
+            'Joke',
+            'Gbenga',
+            'Timi',
+            'Ajayi',
+        ];
 
-		return $response->withJson($users, 200);
-	}
+        return $response->withJson($users, 200);
+    }
 }
